73d44841984190e225d1e8a4697a2c201f356eab
wrathchild14 <jovanprodanov123@gmail.com>
2023-04-04 18:01:18 +0200

    Add initial implementation of UNet

---
+++ segmentation/unet.py
@@ -0,0 +1,79 @@
+from torch import nn, cat
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+
+        self.down1 = EncodeDown(in_channels, 64)
+        self.down2 = EncodeDown(64, 128)
+        self.down3 = EncodeDown(128, 256)
+        self.down4 = EncodeDown(256, 512)
+
+        self.double_conv = DoubleConv(512, 1024)
+
+        self.up1 = DecodeUp(1024, 512)
+        self.up2 = DecodeUp(512, 256)
+        self.up3 = DecodeUp(256, 128)
+        self.up4 = DecodeUp(128, 64)
+
+        self.output = nn.Conv2d(64, out_channels, kernel_size=1, padding=0)
+
+    def forward(self, x):
+        s1, p1 = self.down1(x)
+        s2, p2 = self.down2(p1)
+        s3, p3 = self.down3(p2)
+        s4, p4 = self.down4(p3)
+        b = self.double_conv(p4)
+        d1 = self.up1(b, s4)
+        d2 = self.up2(d1, s3)
+        d3 = self.up3(d2, s2)
+        d4 = self.up4(d3, s1)
+        output = self.output(d4)
+        return output
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+
+        return x
+
+
+class EncodeDown(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = DoubleConv(in_channels, out_channels)
+        self.pool = nn.MaxPool2d((2, 2))
+
+    def forward(self, inputs):
+        x = self.conv(inputs)
+        pool = self.pool(x)
+        return x, pool
+
+
+class DecodeUp(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2, padding=0)
+        self.conv = DoubleConv(out_channels * 2, out_channels)
+
+    def forward(self, x, skip):
+        x = self.up(x)
+        x = cat([x, skip], axis=1)
+        x = self.conv(x)
+        return x
