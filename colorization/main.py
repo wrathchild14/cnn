11c49417fd783029ad7343805ab1acbda5c02393
wrathchild14 <jovanprodanov123@gmail.com>
2023-04-04 23:08:38 +0200

    Use same code in every main

--- colorization/main.py
+++ colorization/main.py
@@ -4,14 +4,12 @@ from colorization.utils import ColorizationDataset, train, test
 from segmentation.unet import UNet
 
 if __name__ == '__main__':
-    trainset = ColorizationDataset(train=True)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=4)
+    net = UNet(in_channels=1, out_channels=3, skip_connection=True)
 
-    testset = ColorizationDataset(train=False)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                             shuffle=False, num_workers=4)
+    train_set = ColorizationDataset(train=True)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=4, shuffle=True, num_workers=4)
+    test_set = ColorizationDataset(train=False)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=4, shuffle=False, num_workers=4)
 
-    net = UNet(in_channels=1, out_channels=3, colorization=True)
-    train(net, train_data=[trainset, trainloader], epochs=5)
-    test(net, testset)
+    train(net, train_data=[train_set, train_loader])
+    test(net, test_set)
--- segmentation/main.py
+++ segmentation/main.py
@@ -5,15 +5,13 @@ from segmentation.unet import UNet
 from segmentation.utils import SegmentationDataset, train, test
 
 if __name__ == '__main__':
-    # net = ResNet18FCN(in_channels=3, out_channels=13)
-    net = UNet(in_channels=3, out_channels=13)
-    trainset = SegmentationDataset(train=True)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=4)
+    net = ResNet18FCN(in_channels=3, out_channels=13)
+    # net = UNet(in_channels=3, out_channels=13)
 
-    testset = SegmentationDataset(train=False)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                             shuffle=False, num_workers=4)
+    train_set = SegmentationDataset(train=True)
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=4, shuffle=True, num_workers=4)
+    test_set = SegmentationDataset(train=False)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=4, shuffle=False, num_workers=4)
 
-    train(net=net, train_data=[trainset, trainloader])
-    test(net=net, testset=testset)
+    train(net=net, train_data=[train_set, train_loader])
+    test(net=net, testset=test_set)
